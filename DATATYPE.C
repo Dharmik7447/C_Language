69296c28d74a5f337271394764901df1c8af5c1e
omprakash1907 <omprakashkjat19@gmail.com>
2025-05-07 17:10:22 +0530

    first commit

---
+++ DATATYPE.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+main(){
+  int a,b,c;
+  float d;
+
+
+
+  clrscr();
+  a=5;
+  b=4;
+  c=a+b;
+  d=3.14;
+  printf("Sum of %d and %d is %d\n",a,b,c);
+  printf("value of pie is %f",d);
+  getch();
+}
+
