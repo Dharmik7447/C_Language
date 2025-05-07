197a9390667a37032fec0546bd485b9d651fd639
omprakash1907 <omprakashkjat19@gmail.com>
2025-05-07 17:29:22 +0530

    third commit

--- DATATYPE.C
+++ DATATYPE.C
@@ -1,17 +1,15 @@
 #include<stdio.h>
 #include<conio.h>
 
-main(){
+main()
+{
   int a,b,c;
   float e;
-
-
-
-  clrscr();
+ clrsrc();
   a=5;
   b=4;
   c=a+b;
-  d=3.14;
+  e=3.14;
   printf("Sum of %d and %d is %d\n",a,b,c);
   printf("value of pie is %f",e);
   getch();
